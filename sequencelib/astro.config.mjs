6e73f8ef3f12399b00ec6d88776af373bd4cd7fd
Walter Moreira <walter@waltermoreira.net>
2025-10-09 20:26:19 -0500

    Add starlight

--- sequencelib/astro.config.mjs
+++ sequencelib/astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   cacheDir: ".astro",
+
   vite: {
     cacheDir: ".vite",
   },
+
+  integrations: [
+    starlight({
+      title: "Sequencelib",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/provables/sequencelib",
+        },
+      ],
+    }),
+  ],
 });
