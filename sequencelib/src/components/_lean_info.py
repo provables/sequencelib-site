22f61b497a25deb9035bbcbe7aa789323a29c8fc
Walter Moreira <walter@waltermoreira.net>
2025-10-14 18:13:52 -0500

    Display number of sequences and theorems

--- sequencelib/src/components/_lean_info.py
+++ sequencelib/src/components/_lean_info.py
@@ -15,10 +15,18 @@ LEAN_INFO = json.load(open(SEQUENCELIB_LEAN_INFO))
 def list_of_ids():
     return [int(v[1:]) for v in chain(*(x.keys() for x in LEAN_INFO.values()))]
 
+def list_of_theorems():
+    for mod, tags in LEAN_INFO.items():
+        for tag, seq in tags.items():
+            for decl, thms in seq["decls"].items():
+                yield from thms.get("thms", [])
 
 # data = json.load(sys.stdin)
+ids = list_of_ids()
 result = {
     "python": sys.executable,
-    "ids": list_of_ids(),
+    "ids": ids,
+    "num_seqs": len(ids),
+    "num_thms": len(list(list_of_theorems()))
 }
 print(json.dumps(result))
